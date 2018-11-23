c1d2f048654f73482657eafd0aaa74f1dd3ad784
贾建宇 <jiajianyu@goldmantis.com>
2018-11-23 15:43:31 +0800

    优化

--- service-feign/src/test/java/com/jjy/servicefeign/ServiceFeignApplicationTests.java
+++ service-feign/src/test/java/com/jjy/servicefeign/ServiceFeignApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SericeFeignApplicationTests {
+public class ServiceFeignApplicationTests {
 
 	@Test
 	public void contextLoads() {
