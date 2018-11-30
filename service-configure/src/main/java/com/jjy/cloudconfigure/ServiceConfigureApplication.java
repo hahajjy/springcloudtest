22aeb4382091b960ba2f45e48ff3b576e72584f8
贾建宇 <jiajianyu@goldmantis.com>
2018-11-30 14:29:40 +0800

    结构调整

--- service-configure/src/main/java/com/jjy/cloudconfigure/ServiceConfigureApplication.java
+++ service-configure/src/main/java/com/jjy/cloudconfigure/ServiceConfigureApplication.java
@@ -2,16 +2,15 @@ package com.jjy.cloudconfigure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
-public class CloudConfigureApplication {
+public class ServiceConfigureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudConfigureApplication.class, args);
+		SpringApplication.run(ServiceConfigureApplication.class, args);
 	}
 }
